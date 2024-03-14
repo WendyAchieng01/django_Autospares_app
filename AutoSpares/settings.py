4a6861dc8360cb3052e930d6314824d5e014e3e0
WendyAchieng01 <wendyachieng98@gmail.com>
2024-03-14 16:44:01 +0300

    add site corrections

--- AutoSpares/settings.py
+++ AutoSpares/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*5u$#&&2#xk657m9&d)&tzi0+0mt(68-r%u-xx=i!^4z^!)v8z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
